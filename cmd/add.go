b23b69b2ce30992db9dfbb9a3016235934e71e87
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-05-04 22:31:36 +0200

    fixed the add command

--- cmd/add.go
+++ cmd/add.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"encoding/json"
+	"io"
 	"log/slog"
 	"os"
 
@@ -50,8 +51,8 @@ to quickly create a Cobra application.`,
 			os.Exit(1)
 		}
 
-		// Write task to file
-		file, err := os.OpenFile("./data/tasks.json", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		// Read tasks from file
+		file, err := os.OpenFile("./data/tasks.json", os.O_CREATE|os.O_RDONLY, 0644)
 		if err != nil {
 			slog.Error("error reading or creating tasks.json file", "err", err)
 		}
@@ -59,11 +60,20 @@ to quickly create a Cobra application.`,
 
 		decoder := json.NewDecoder(file)
 		tasks := []model.Task{}
-		decoder.Decode(&tasks)
+		err = decoder.Decode(&tasks)
+		if err != nil && err != io.EOF {
+			slog.Error("Error decoding JSON file", "err", err)
+			os.Exit(1)
+		}
 
 		tasks = append(tasks, newTask)
 
-		encoder := json.NewEncoder(file)
+		writeFile, err := os.OpenFile("./data/tasks.json", os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			slog.Error("error writing tasks to file", "err", err)
+			os.Exit(1)
+		}
+		encoder := json.NewEncoder(writeFile)
 		err = encoder.Encode(tasks)
 		if err != nil {
 			slog.Error("error writing to tasks.json", "err", err)
