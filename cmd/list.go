f9449e316dc4e2cfd7d6c9e12c610c3bd55ce997
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-05-04 22:14:09 +0200

    added the list command that lists all tasks

--- cmd/list.go
+++ cmd/list.go
@@ -1,12 +1,17 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"os"
+	"strconv"
+	"time"
 
+	"github.com/matthieukhl/task-tracker/internal/model"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
@@ -21,7 +26,41 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		file, err := os.OpenFile("data/tasks.json", os.O_RDONLY, 0644)
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		tasks := []model.Task{}
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&tasks)
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		data := [][]string{
+			{"ID", "Title", "Description", "Status", "Created at", "Updated at"},
+		}
+
+		for _, task := range tasks {
+			taskInfo := []string{
+				strconv.Itoa(task.ID),
+				task.Title,
+				task.Description,
+				task.Status,
+				task.CreatedAt.Format(time.RFC822),
+				task.UpdatedAt.Format(time.RFC822),
+			}
+
+			data = append(data, taskInfo)
+		}
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.Header(data[0])
+		table.Bulk(data[1:])
+		table.Render()
 	},
 }
 
